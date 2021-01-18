0897d843bed6c8182e8a1775a6aa430448272e14
DAVID <tsiba1@walla.co.il>
2021-01-18 22:33:54 +0200

    portfolio_back

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def main():
         new_order = Controller.broker.binance_client.order_limit_buy(symbol="BNBBTC",quantity=1,price=0.0012)
     except:
         print("No Balance")
+
     all_prices = Controller.broker.getAllTickers()
     all_assets = Controller.broker.getTotalBalances()
     total_usd_value = Controller.broker.getTotalUsdValue()
+
+    
 
     pass
 
